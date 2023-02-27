fe60bc6aef52959ff0b12f77f5d5165c56801cc6
Prodromos Pantos <prpanto@gmail.com>
2023-02-28 00:02:55 +0200

    Init app

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="relative h-screen flex justify-center items-center bg-gray-100">
+    <button class="px-4 py-2 bg-blue-600 rounded-md text-white mr-2" @click="toast.add({ message: 'Toast message!' })">Add toast</button>
+    <button class="px-4 py-2 bg-blue-600 rounded-md text-white" @click="showAlert ^= true">Show Alert</button>
+
+    <Toast />
+
+    <div class="absolute top-4 right-4 space-y-4">
+      <Alert
+        :show="!!showAlert"
+        :on-dismiss="() => (showAlert = false)"
+        title="This is my awesome alert!"
+        class="max-w-md w-full"
+      />
+      <Alert
+        intent="success"
+        :show="!!showAlert"
+        :on-dismiss="() => (showAlert = false)"
+        title="This is my awesome alert!"
+        class="max-w-md w-full"
+      />
+      <Alert
+        intent="warning"
+        :show="!!showAlert"
+        :on-dismiss="() => (showAlert = false)"
+        title="This is my awesome alert!"
+        class="max-w-md w-full"
+      />
+      <Alert
+        intent="danger"
+        :show="!!showAlert"
+        :on-dismiss="() => (showAlert = false)"
+        title="This is my awesome alert!"
+        class="max-w-md w-full"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Alert from '@/components/Alert.vue'
+import Toast from '@/components/Toast.vue'
+import toast from '@/store/toast'
+
+const showAlert = ref(false)
+</script>
---
+++ src/components/Toast.vue
@@ -0,0 +1,28 @@
+<template>
+  <TransitionGroup 
+    tag="div"
+    enter-from-class="translate-x-full opacity-0"
+    enter-active-class="duration-500"
+    leave-active-class="duration-500"
+    leave-to-class="translate-x-full opacity-0"
+    class="fixed top-4 right-4 z-50 w-full max-w-xs space-y-4"
+  >
+    <ToastListItem
+      v-for="(item, index) in toast.items"
+      :key="item.key"
+      :message="item.message"
+      @remove="remove(index)"
+    />
+  </TransitionGroup>
+</template>
+
+<script setup>
+import { onUnmounted } from 'vue'
+import ToastListItem from '@/components/ToastItem.vue'
+import toast from '@/store/toast'
+
+const removeFinishEventListener = () => toast.add({ message: 'Toast message!' })
+const remove = index => toast.remove(index)
+
+onUnmounted(() => removeFinishEventListener())
+</script>
